80033cd6097cd8efcacd161eadae64244612bc17
Jetze Janssens <jetzejanssens@MacBook-Pro-van-Jetze-2.local>
2026-07-09 00:11:42 +0200

    aanpassen vondsten knop

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -89,7 +89,7 @@ export default function HomePage() {
             </div>
             <div>
               <p style={{ fontSize: '15px', fontWeight: '800', color: 'white', margin: '0 0 2px', letterSpacing: '-0.2px' }}>
-                Vondst melden
+                Vondsten
               </p>
               <p style={{ fontSize: '12px', color: 'rgba(255,255,255,0.7)', margin: 0 }}>
                 Registreer via h-a-l.be
